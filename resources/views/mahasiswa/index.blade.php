87b6dca919788b02998af0ffaedcab9144f36a2b
Ge-Backspace <ismailgo441@gmail.com>
2020-02-25 17:14:05 +0700

    Menambahkan page Mahasiswa dengan membuat folder dan controller khusus untuk menampilkan data dari database

--- resources/views/mahasiswa/index.blade.php
+++ resources/views/mahasiswa/index.blade.php
@@ -1,10 +1,10 @@
 @extends('layout/main')
-@section('title', 'Hai nona cantik')
+@section('title', 'Daftar Mahasiswa')
 @section('container')
 	<div class="container">
 		<div class="row">
 			<div class="col-10">
-				<h1 class="mt-3">Page Mahasiswa</h1>
+				<h1 class="mt-3">Daftar Mahasiswa</h1>
 			</div>
 		</div>
 	</div>
--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,5 @@
 
 Route::get('/', 'PagesController@home');
 Route::get('/about', 'PagesController@about');
+
+Route::get('/mahasiswa', 'MahasiswaController@index');
